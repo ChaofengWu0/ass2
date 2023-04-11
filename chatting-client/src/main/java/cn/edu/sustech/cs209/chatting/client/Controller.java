86e63e40eaf262046d9694ae185807c8bd4c9700
ChaofengWu0 <1720751884@qq.com>
2023-04-11 21:51:22 +0800

    修改之前

--- chatting-client/src/main/java/cn/edu/sustech/cs209/chatting/client/Controller.java
+++ chatting-client/src/main/java/cn/edu/sustech/cs209/chatting/client/Controller.java
@@ -2,6 +2,8 @@ package cn.edu.sustech.cs209.chatting.client;
 
 import cn.edu.sustech.cs209.chatting.common.Message;
 import javafx.application.Platform;
+import javafx.beans.value.ObservableValue;
+import javafx.beans.value.ObservableValueBase;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -48,7 +50,10 @@ public class Controller implements Initializable {
     private ObjectInputStream in;
     private Socket socket;
     private String username;
+    private String speakTo;
     private ClientService clientService;
+
+    private List<String> actives;
 
     public void check() throws IOException {
         // 发送给服务器，让服务器去查询当前username有没有在数据库中
@@ -62,6 +67,7 @@ public class Controller implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         // this.arrayList = new ArrayList<>();
+        this.actives = new ArrayList<>();
         selectedUsers = new ArrayList<>();
         searchActiveLock = new ReentrantLock();
         // inLock = new ReentrantLock();
@@ -108,7 +114,21 @@ public class Controller implements Initializable {
             System.out.println("Invalid username " + input + ", exiting");
             Platform.exit();
         }
+
+        chatList.getSelectionModel()
+                .selectedItemProperty()
+                .addListener((o, oldValue, newValue) -> {
+                            // 捕获到了，就要把右边的改过去
+                            speakTo = newValue;
+                            // 查询已经向他发送的信息，把那个list传入就行。
+                            // this.chatContentList.setItems();
+                        }
+                );
+
+
         chatContentList.setCellFactory(new MessageCellFactory());
+
+
     }
 
     public void searchHowMany() throws IOException {
@@ -248,149 +268,154 @@ public class Controller implements Initializable {
     }
 
 
-    public static class ClientService implements Runnable {
-        private final Socket socket;
-
-        private ReentrantLock searchActiveLock;
-        private ObjectInputStream in;
-        private ObjectOutputStream out;
-        private Integer type;
-
-        private Boolean flagCheckLogin;
-
-        private Boolean flagSearchActives;
-        private Boolean login;
-        private String username;
-        private List<String> actives;
-        private List<String> hasLinks;
-        ObservableList<String> observableList;
-
-        public ClientService(Socket socket, String username, ObjectInputStream in, ObjectOutputStream out, ReentrantLock searchActiveLock) {
-            this.searchActiveLock = searchActiveLock;
-            this.socket = socket;
-            this.username = username;
-            this.in = in;
-            this.out = out;
-            this.actives = new ArrayList<>();
-            this.login = false;
-            this.flagCheckLogin = false;
-            this.flagSearchActives = false;
-            this.hasLinks = new ArrayList<>();
-            this.observableList = FXCollections.observableArrayList(hasLinks);
-
-        }
-
-        @Override
-        public void run() {
-            try {
-                try {
-                    doClientService();
-                } finally {
-                    socket.close();
-                }
-            } catch (IOException | ClassNotFoundException e) {
-                e.printStackTrace();
-            }
-        }
-
-        public synchronized void doClientService() throws IOException, ClassNotFoundException {
-            while (true) {
-                Object obj = in.readObject();
-                if (obj == null) continue;
-                Message message = (Message) obj;
-                int type = message.getType();
-                switch (type) {
-                    case 1: {
-                        System.out.println("client case 1");
-                        whetherLogin(message);
-                        this.flagCheckLogin = true;
-                        break;
-                    }
-                    case 3: {
-                        System.out.println("client case 3");
-                        // 获取到了目前有多少人在线
-                        getHowManyActives(message);
-                        break;
-                    }
-                    case 5: {
-                        System.out.println("client case 5");
-                        addLinks(message);
-                        break;
-                    }
-                    // case 7: {
-                    //     System.out.println("client case 7");
-                    //     handShaking2(message);
-                    //     break;
-                    // }
-                }
-            }
-        }
-
-        public void whetherLogin(Message message) {
-            if (message.getData().equals("success")) {
-                this.login = true;
-            } else {
-                this.login = false;
-                System.out.println("你的账号已在其他地方登录或密码错误");
-            }
-        }
-
-        public void getHowManyActives(Message message) {
-            System.out.println(flagSearchActives);
-            System.out.println("before set flag");
-            searchActiveLock.lock();
-            this.setActives(message.getActives());
-            this.setFlagSearchActives(true);
-            System.out.println("after flag");
-            System.out.println("getHowManyActives : " + flagSearchActives);
-            searchActiveLock.unlock();
-        }
-
-        public void addLinks(Message message) {
-            this.hasLinks.add(message.getData());
-        }
-
-        public Boolean getFlagSearchActives() {
-            return flagSearchActives;
-        }
-
-        public void setFlagSearchActives(Boolean flagSearchActives) {
-            this.flagSearchActives = flagSearchActives;
-        }
-
-        public Integer getType() {
-            return type;
-        }
-
-        public void setType(Integer type) {
-            this.type = type;
-        }
-
-        public List<String> getActives() {
-            return actives;
-        }
-
-        public void setActives(List<String> actives) {
-            this.actives = actives;
-        }
-
-        public Boolean getLogin() {
-            return login;
-        }
-
-        public void setLogin(Boolean login) {
-            this.login = login;
-        }
-
-        public Boolean getFlagCheckLogin() {
-            return flagCheckLogin;
-        }
-
-        public void setFlagCheckLogin(Boolean flagCheckLogin) {
-            this.flagCheckLogin = flagCheckLogin;
-        }
-
-    }
+    // public class ClientService implements Runnable {
+    //     private final Socket socket;
+    //
+    //     private ReentrantLock searchActiveLock;
+    //     private ObjectInputStream in;
+    //     private ObjectOutputStream out;
+    //     private Integer type;
+    //
+    //     private Boolean flagCheckLogin;
+    //
+    //     private Boolean flagSearchActives;
+    //     private Boolean login;
+    //     private String username;
+    //     private List<String> actives;
+    //     private List<String> hasLinks;
+    //     ObservableList<String> observableList;
+    //
+    //     public ClientService(Socket socket, String username, ObjectInputStream in, ObjectOutputStream out, ReentrantLock searchActiveLock) {
+    //         this.searchActiveLock = searchActiveLock;
+    //         this.socket = socket;
+    //         this.username = username;
+    //         this.in = in;
+    //         this.out = out;
+    //         this.actives = new ArrayList<>();
+    //         this.login = false;
+    //         this.flagCheckLogin = false;
+    //         this.flagSearchActives = false;
+    //         this.hasLinks = new ArrayList<>();
+    //         this.observableList = FXCollections.observableArrayList(hasLinks);
+    //
+    //     }
+    //
+    //     @Override
+    //     public void run() {
+    //         try {
+    //             try {
+    //                 doClientService();
+    //             } finally {
+    //                 socket.close();
+    //             }
+    //         } catch (IOException | ClassNotFoundException e) {
+    //             e.printStackTrace();
+    //         }
+    //     }
+    //
+    //     public synchronized void doClientService() throws IOException, ClassNotFoundException {
+    //         while (true) {
+    //             Object obj = in.readObject();
+    //             if (obj == null) continue;
+    //             Message message = (Message) obj;
+    //             int type = message.getType();
+    //             switch (type) {
+    //                 case 1: {
+    //                     System.out.println("client case 1");
+    //                     whetherLogin(message);
+    //                     this.flagCheckLogin = true;
+    //                     break;
+    //                 }
+    //                 case 3: {
+    //                     System.out.println("client case 3");
+    //                     // 获取到了目前有多少人在线
+    //                     getHowManyActives(message);
+    //                     // List<String> list = message.getActives();
+    //                     // for (int i = 0; i < list.size(); i++) {
+    //                     //     System.out.println(list.get(i));
+    //                     // }
+    //                     // Platform.runLater(()->Controller.this.actives = list);
+    //                     break;
+    //                 }
+    //                 case 5: {
+    //                     System.out.println("client case 5");
+    //                     addLinks(message);
+    //                     break;
+    //                 }
+    //                 // case 7: {
+    //                 //     System.out.println("client case 7");
+    //                 //     handShaking2(message);
+    //                 //     break;
+    //                 // }
+    //             }
+    //         }
+    //     }
+    //
+    //     public void whetherLogin(Message message) {
+    //         if (message.getData().equals("success")) {
+    //             this.login = true;
+    //         } else {
+    //             this.login = false;
+    //             System.out.println("你的账号已在其他地方登录或密码错误");
+    //         }
+    //     }
+    //
+    //     public void getHowManyActives(Message message) {
+    //         System.out.println(flagSearchActives);
+    //         System.out.println("before set flag");
+    //         searchActiveLock.lock();
+    //         this.setActives(message.getActives());
+    //         this.setFlagSearchActives(true);
+    //         System.out.println("after flag");
+    //         System.out.println("getHowManyActives : " + flagSearchActives);
+    //         searchActiveLock.unlock();
+    //     }
+    //
+    //     public void addLinks(Message message) {
+    //         this.hasLinks.add(message.getData());
+    //     }
+    //
+    //     public Boolean getFlagSearchActives() {
+    //         return flagSearchActives;
+    //     }
+    //
+    //     public void setFlagSearchActives(Boolean flagSearchActives) {
+    //         this.flagSearchActives = flagSearchActives;
+    //     }
+    //
+    //     public Integer getType() {
+    //         return type;
+    //     }
+    //
+    //     public void setType(Integer type) {
+    //         this.type = type;
+    //     }
+    //
+    //     public List<String> getActives() {
+    //         return actives;
+    //     }
+    //
+    //     public void setActives(List<String> actives) {
+    //         this.actives = actives;
+    //     }
+    //
+    //     public Boolean getLogin() {
+    //         return login;
+    //     }
+    //
+    //     public void setLogin(Boolean login) {
+    //         this.login = login;
+    //     }
+    //
+    //     public Boolean getFlagCheckLogin() {
+    //         return flagCheckLogin;
+    //     }
+    //
+    //     public void setFlagCheckLogin(Boolean flagCheckLogin) {
+    //         this.flagCheckLogin = flagCheckLogin;
+    //     }
+    //
+    // }
 
 
 }
